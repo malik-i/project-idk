b3849322d3cec4403fb4f7f27fb6c1389161a4a6
malik iscandari <malikiscan@gmail.com>
2019-05-18 18:33:00 -0700

    added the routers

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ class App extends Component {
   render(){
     return(
       <React.Fragment>
-
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
       </React.Fragment>
     )
   }
